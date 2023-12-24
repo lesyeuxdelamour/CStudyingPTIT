e2316bab5df0faf7d00aaa55840a7a77cfd758be
Bad Liar <transytien239@gmail.com>
2023-12-24 19:49:15 +0700

    Update

--- mixingstrings.cpp
+++ mixingstrings.cpp
@@ -1,52 +1,52 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 
 void mix(char *s1, char *s2, char *s12, int t)
 {
-	for(int i = 0, j = 0; i < t, j < 2*t; i++, j+=2)
+	for (int i = 0, j = 0; i < t, j < 2 * t; i++, j += 2)
 	{
-		s12[j] = s1[i];
-		s12[j+1] = s2[i];
+		s12[j] = s2[i];
+		s12[j + 1] = s1[i];
 	}
-	s12[2*t] = '\0';
+	s12[2 * t] = '\0';
 }
 
 void split(char *s1, char *s2, char *s12, int t)
 {
-	for(int i = 0; i < t; ++i)
+	for (int i = 0; i < t; ++i)
 	{
 		s1[i] = s12[i];
-		s2[i] = s12[i+t];
+		s2[i] = s12[i + t];
 	}
-}	
+}
 
 int solve(int t)
 {
-	char s1[t], s2[t], s12[2*t+1];
+	char s1[t + 1], s2[t + 1], s12[2 * t + 1];
 	scanf("%s%s%s", s1, s2, s12);
-	char c1[t], c2[t], c12[2*t+1];
-	strcpy(c1, s1); strcpy(c2, s2);
+	char c1[t + 1], c2[t + 1], c12[2 * t + 1];
+	strcpy(c1, s1);
+	strcpy(c2, s2);
 	int count = 0;
-	while(1)
+	while (1)
 	{
 		count++;
-		mix(c1, c2, c12, t); 
-		if(strcmp(s12, c12) == 0)
+		mix(c1, c2, c12, t);
+		if (strcmp(s12, c12) == 0)
 			return count;
 		split(c1, c2, c12, t);
-		if(!strcmp(c1, s1) && !strcmp(c2, s2))
-			break;
+		if (strcmp(c1, s1) == 0 && strcmp(c2, s2) == 0)
+			return -1;
 	}
-	return -1;
 }
 
 int main()
 {
 	int t = -1;
-	while(t)
+	while (t)
 	{
 		scanf("%d\n", &t);
-		if(t)
+		if (t)
 			printf("%d\n", solve(t));
 	}
 	return 0;
